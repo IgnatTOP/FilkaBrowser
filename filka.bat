e027069740395822471346f3e2ab6a4e7e4b6cdf
Ignat <ignattop@users.noreply.github.com>
2026-06-16 02:16:48 +0300

    fix: bat keeps console open to show errors

--- filka.bat
+++ filka.bat
@@ -1,4 +1,8 @@
 @echo off
 set QTWEBENGINE_DISABLE_SANDBOX=1
 set QTWEBENGINE_CHROMIUM_FLAGS=--disable-gpu-compositing
-start "" "%~dp0filka.exe" %*
+set QT_DEBUG_PLUGINS=1
+"%~dp0filka.exe" %*
+echo.
+echo === Process exited with code %ERRORLEVEL% ===
+pause
